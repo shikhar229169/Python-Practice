02843fc92f7ae09f1394a4e18599e74f972a6634
shikhar <shikharagarwale7@gmail.com>
2023-07-30 20:11:53 +0530

    ✨init

--- timeConv.py
+++ timeConv.py
@@ -4,17 +4,26 @@
 def timeConv(currTime):
     # I don't like magic numbers
     OFFSET = 12
-    MAX_HOURS = 24
 
     hr = int(currTime[0:2])
     min = int(currTime[3:5])
 
-    if "PM" in currTime:
-        hr = (hr + OFFSET) % MAX_HOURS
+    if "PM" in currTime and hr != 12:
+        hr = (hr + OFFSET)
+    elif "AM" in currTime and hr == 12:
+        hr = 0
     
-    return str(hr) + ':' + str(min)
+    convTime = ""
+
+    convTime += (str(hr // 10))
+    convTime += (str(hr % 10))
+    convTime += (":")
+    convTime += (str(min // 10))
+    convTime += (str(min % 10))
+
+    return convTime
 
 
-myTime = "11:53 PM"
+myTime = "01:53 PM"
 _24_FORMAT = timeConv(myTime)    
 print(_24_FORMAT)
